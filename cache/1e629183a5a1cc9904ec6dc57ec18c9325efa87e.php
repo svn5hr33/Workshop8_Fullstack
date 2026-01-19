1dfc2988fb2689e1c6d5af6be6e3f196ca015983
svn5hr33 <ssunshree@gmail.com>
2026-01-19 16:57:13 +0545

    upload

---
+++ cache/1e629183a5a1cc9904ec6dc57ec18c9325efa87e.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $__env->yieldContent('title'); ?></title>
+</head>
+<body>
+<h2>Simple Employee CRUD</h2>
+<hr>
+<?php echo $__env->yieldContent('content'); ?>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\Workshop8\app\views/layout.blade.php ENDPATH**/ ?>
---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$db = 'workshop_db';
+$user = 'root';
+$pass = '';
+try {
+$pdo = new PDO(
+"mysql:host=$host;dbname=$db;charset=utf8",
+$user,
+$pass,
+[
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]
+);
+} catch (PDOException $e) {
+die("Database connection failed");
+}
